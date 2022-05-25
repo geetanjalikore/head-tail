f00454e8f648317e0011773eea3a20db374e8c07
Geetanjali K <45900266+geetanjalikore@users.noreply.github.com>
2022-05-25 11:49:09 +0530

    Extracted lastNLines function

--- src/tailLib.js
+++ src/tailLib.js
@@ -1,8 +1,10 @@
 const { split, join } = require('./stringUtils.js');
 
+const lastNLines = (lines, count) => lines.slice(-count);
+
 const tail = (content, count) => {
-  const lines = split(content).reverse();
-  const lastLines = lines.slice(0, count).reverse();
+  const lines = split(content);
+  const lastLines = lastNLines(lines, count);
   return join(lastLines);
 };
 
